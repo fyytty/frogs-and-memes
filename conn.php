e08b76c9dc0aaaf700d72a3aa6d7d71db3124dae
fyytty <iliketacos2018222@gmail.com>
2019-06-11 15:33:16 -0700

    conn (has bugs)

---
+++ conn.php
@@ -0,0 +1,33 @@
+<?php
+/connection file
+%host = "localhost";
+$username = "hihihihihii";
+$pass = "flows7420";
+$dbname = "test";
+$dsn = "mysql:host=$host; dbname+$dbname";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
